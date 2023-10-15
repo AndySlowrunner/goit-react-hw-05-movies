510472fad4b3cd82113266f9e25a56a8db9e71e2
Andy <andyfartushniy@gmail.com>
2023-10-16 01:08:07 +0300

    Added API

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 
-// const API_KEY = '233c7f71dccea172993da041a5656481';
+const API_KEY = '233c7f71dccea172993da041a5656481';
 
 export const getTrendingFilms = async() => {
-    const response = await axios.get('https://api.themoviedb.org/3/trending/all/day?language=en-US')
-    return response
+    
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&language=en-US`);
+        return response.data;
+    } catch (error) {
+
+        console.error("Помилка при отриманні трендових фільмів:", error);
+        throw error;
+    }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
+import { getTrendingFilms } from "components/Service"
+import { useEffect, useState } from "react"
+
 export const Home = () => {
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        getTrendingFilms()
+            .then((response) => {
+                const trendingFilms = response.results;
+                setFilms(trendingFilms);
+            })
+            .catch((error) => {
+                console.error('Помилка при отриманні фільмів:', error);
+            });
+    }, []);
+
     return (
         <main>
             <h1>Trending today:</h1>
             <ul>
-                <li>Pop Film #1</li>
-                <li>Pop Film #2</li>
-                <li>Pop Film #3</li>
-                <li>Pop Film #4</li>
-                <li>Pop Film #5</li>
+                {films.map(film =>
+                    <li key={film.id}>
+                        <p>{film.title || film.name}</p>
+                    </li>
+                )}
             </ul>
         </main>
     )
