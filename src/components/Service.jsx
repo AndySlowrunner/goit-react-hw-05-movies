ce431c5e48956cb2483064a83099436b4ca35813
Andy <andyfartushniy@gmail.com>
2023-10-17 23:47:33 +0300

    Added movieDet

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -13,3 +13,15 @@ export const getTrendingFilms = async() => {
         throw error;
     }
 }
+
+export const getFilmDetails = async(id) => {
+    
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`);
+        return response.data;
+    } catch (error) {
+
+        console.error("Помилка при отриманні інфо про фільм:", error);
+        throw error;
+    }
+}
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,17 +1,36 @@
 // import { useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { getFilmDetails } from "components/Service"
 
 export const MovietDetails = () => {
-    // const { id } = useParams;
-    // const [movies, setMovies] = useState();
+    const { id } = useParams();
+    const [movie, setMovie] = useState({});
+    const { title, overview, genres } = movie;
 
-    // const getMovieById = (movieId) => {
-    //     return movies.find(movie => movie.id === movieId);
-    // };
-    // const movie = getMovieById(id);
+    useEffect(() => {
+        getFilmDetails(id)
+            .then((response) => {
+                const filmDetails = response;
+                setMovie(filmDetails);
+            })
+            .catch((error) => {
+                console.error('Помилка при отриманні фільмів:', error);
+            });
+    }, [id]);
+
     return (
         <main>
-            <div>Details</div>
+            <h2>{title}</h2>
+            <h3>Overview</h3>
+            <p>{overview}</p>
+            <h3>Genres</h3>
+            <ul>
+            {genres.map(genre => (
+                <li key={genre.id}>{genre.name}</li>
+            ))}
+            </ul>            
+            <p>Information</p>
         </main>
     )
     
